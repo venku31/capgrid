fc1cdf697cff8379e11ea44c18f5478a1193eb37
aashishvashisht6 <aashishvashisht6@gmail.com>
2023-06-24 10:02:51 +0530

    fix: get actual stock in capgrid stock field

--- capgrid/capgrid/doctype/picklist/picklist.js
+++ capgrid/capgrid/doctype/picklist/picklist.js
@@ -1,28 +1,28 @@
 // Copyright (c) 2023, Capgrid Solutions and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('PickList Details', {
-qty: function(frm, cdt, cdn) {
-total_qty(frm, cdt, cdn);
-	},
-picked_qty: function(frm, cdt, cdn) {
-total_qty(frm, cdt, cdn);
-	},
-validate: function(frm, cdt, cdn) {
-total_qty(frm, cdt, cdn);
-	},
+frappe.ui.form.on("PickList Details", {
+  qty: function (frm, cdt, cdn) {
+    total_qty(frm, cdt, cdn);
+  },
+  picked_qty: function (frm, cdt, cdn) {
+    total_qty(frm, cdt, cdn);
+  },
+  validate: function (frm, cdt, cdn) {
+    total_qty(frm, cdt, cdn);
+  },
 });
-frappe.ui.form.on('PickList', {
-validate: function(frm, cdt, cdn) {
-total_qty(frm, cdt, cdn);
-fetch_actual_qty(frm, cdt, cdn);
-	},
+frappe.ui.form.on("PickList", {
+  validate: function (frm, cdt, cdn) {
+    total_qty(frm, cdt, cdn);
+    fetch_actual_qty(frm, cdt, cdn);
+  },
 });
 
 // frappe.ui.form.on("PickList", "validate", function(frm, cdt, cdn) {
 // 	$.each(frm.doc.details || [], function(i, d) {
 // 		if(d.part_number){
-// 		console.log("first call")    
+// 		console.log("first call")
 // 		frappe.call({
 // 				method: "capgrid.capgrid.doctype.pick_list_upload.pick_list_upload.get_actual_stock",
 // 				args: {"warehouse":frm.doc.warehouse,"part_number":d.part_number},
@@ -32,37 +32,41 @@ fetch_actual_qty(frm, cdt, cdn);
 // 					frappe.model.set_value(cdt, cdn, d.actual_stock, price);
 // 		d.actual_stock=price
 // 				}
-// 			});	
+// 			});
 // 		}
 // 	   })
 // 	  refresh_field("details");
 // 	})
 
 function total_qty(frm, cdt, cdn) {
-    // estimated_amount: function(frm, cdt, cdn) {
-    var d = locals[cdt][cdn];
-    var total_qty = 0;
-    var total_picked_qty = 0;
-	frm.doc.details.forEach(function(d) { total_qty += d.qty});
-    frm.doc.details.forEach(function(d) { total_picked_qty += d.picked_qty});
-	frm.set_value('total_qty', total_qty);
-    frm.set_value('total_picked_qty', total_picked_qty);
-	cur_frm.set_value("created_by", frappe.session.user);
-	}	
+  // estimated_amount: function(frm, cdt, cdn) {
+  var d = locals[cdt][cdn];
+  var total_qty = 0;
+  var total_picked_qty = 0;
+  frm.doc.details.forEach(function (d) {
+    total_qty += d.qty;
+  });
+  frm.doc.details.forEach(function (d) {
+    total_picked_qty += d.picked_qty;
+  });
+  frm.set_value("total_qty", total_qty);
+  frm.set_value("total_picked_qty", total_picked_qty);
+  cur_frm.set_value("created_by", frappe.session.user);
+}
 
 function fetch_actual_qty(frm, cdt, cdn) {
-    $.each(frm.doc.details || [], function(i, d) {
-        if(d.part_number){
-        frappe.call({
-                method: "capgrid.capgrid.doctype.pick_list_upload.pick_list_upload.get_actual_stock",
-                args: {"warehouse":frm.doc.warehouse,"part_number":d.part_number},
-                callback: function(r) {
-                    console.log(r.message);
-                    var actual_qty = r.message[0].actual_qty
-                    frappe.model.set_value(cdt, cdn, d.actual_stock, actual_qty);
-					d.actual_stock=actual_qty
-                }
-            });	
-        }
-});
+  $.each(frm.doc.details || [], function (i, d) {
+    if (d.part_number) {
+      frappe.call({
+        method: "capgrid.capgrid.doctype.picklist.picklist.get_actual_stock",
+        args: { warehouse: frm.doc.warehouse, part_number: d.part_number },
+        callback: function (r) {
+          console.log(r.message);
+          var actual_qty = r.message.actual_qty;
+          frappe.model.set_value(cdt, cdn, d.actual_stock, actual_qty);
+          d.actual_stock = actual_qty;
+        },
+      });
+    }
+  });
 }
--- capgrid/capgrid/doctype/picklist/picklist.py
+++ capgrid/capgrid/doctype/picklist/picklist.py
@@ -1,18 +1,24 @@
 # Copyright (c) 2023, Capgrid Solutions and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class PickList(Document):
-    pass
-import frappe
-from frappe import _
+    def validate(self):
+        self.total_qty = 0
+        self.total_picked_qty = 0
+        if self.get('details'):
+            for item in self.get('details'):
+                self.total_qty += item.trigger_qty
+                self.total_picked_qty += item.total_picked_qty if item.total_picked_qty else 0
+                item.actual_stock = get_actual_stock(self.warehouse, item.part_number)
+
+    
 
 @frappe.whitelist()
 def get_actual_stock(warehouse,part_number):
     if part_number:
         actual_qty=frappe.db.sql(""" select sum(actual_qty) as actual_qty from tabBin where item_code=%(part_number)s and warehouse=%(warehouse)s """,{"part_number":part_number,"warehouse":warehouse} , as_dict=1)
-        # print("////////",actual_qty)
-        return actual_qty
+        return actual_qty[0].actual_qty
         
