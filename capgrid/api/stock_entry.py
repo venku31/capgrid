7274194d823b109308635edab910272d6b591353
VenkateshNayak <vnayak@creativeadvtech.com>
2024-01-16 17:02:04 +0530

    stock entry diffrence value issue

--- capgrid/api/stock_entry.py
+++ capgrid/api/stock_entry.py
@@ -97,18 +97,77 @@ class CustomStockEntry(StockEntry):
         if self.purpose == "Material Transfer" and self.outgoing_stock_entry:
             self.set_material_request_transfer_status("Completed")
 
+    def set_basic_rate(self, reset_outgoing_rate=True, raise_error_if_no_rate=True):
+        """
+        Set rate for outgoing, scrapped and finished items
+        """
+        # Set rate for outgoing items
+        outgoing_items_cost = self.set_rate_for_outgoing_items(
+            reset_outgoing_rate, raise_error_if_no_rate
+        )
+        finished_item_qty = sum(d.transfer_qty for d in self.items if d.is_finished_item)
+
+        items = []
+        # Set basic rate for incoming items
+        for d in self.get("items"):
+            if d.s_warehouse or d.set_basic_rate_manually:
+                continue
+
+            if d.allow_zero_valuation_rate:
+                d.basic_rate = 0.0
+                items.append(d.item_code)
+
+            elif d.is_finished_item:
+                if self.purpose == "Manufacture":
+                    d.basic_rate = self.get_basic_rate_for_manufactured_item(
+                        finished_item_qty, outgoing_items_cost
+                    )
+                elif self.purpose == "Repack":
+                    d.basic_rate = self.get_basic_rate_for_repacked_items(d.transfer_qty, outgoing_items_cost)
+
+            if not d.basic_rate and not d.allow_zero_valuation_rate:
+                d.basic_rate = get_valuation_rate(
+                    d.item_code,
+                    d.t_warehouse,
+                    self.doctype,
+                    self.name,
+                    d.allow_zero_valuation_rate,
+                    currency=erpnext.get_company_currency(self.company),
+                    company=self.company,
+                    raise_error_if_no_rate=raise_error_if_no_rate,
+                    batch_no=d.batch_no,
+                )
+
+            # do not round off basic rate to avoid precision loss
+            d.basic_rate = flt(d.basic_rate)
+            d.basic_amount = flt(flt(d.transfer_qty) * flt(d.precision("basic_rate")), d.precision("basic_amount"))
+
+        if items:
+            message = ""
+
+            if len(items) > 1:
+                message = _(
+                    "Items rate has been updated to zero as Allow Zero Valuation Rate is checked for the following items: {0}"
+                ).format(", ".join(frappe.bold(item) for item in items))
+            else:
+                message = _(
+                    "Item rate has been updated to zero as Allow Zero Valuation Rate is checked for item {0}"
+                ).format(frappe.bold(items[0]))
+
+            frappe.msgprint(message, alert=True)
+
     def set_rate_for_outgoing_items(self, reset_outgoing_rate=True, raise_error_if_no_rate=True):
         outgoing_items_cost = 0.0
         for d in self.get("items"):
-            if d.s_warehouse and not d.set_basic_rate_manually:
+            if d.s_warehouse:
                 if reset_outgoing_rate:
                     args = self.get_args_for_incoming_rate(d)
                     rate = get_incoming_rate(args, raise_error_if_no_rate)
                     if rate > 0:
                         d.basic_rate = rate
 
-                d.basic_amount = flt(flt(d.transfer_qty) * flt(d.basic_rate), d.precision("basic_amount"))
+                d.basic_amount = flt(flt(d.transfer_qty) * flt(d.precision("basic_rate")), d.precision("basic_amount"))
                 if not d.t_warehouse:
                     outgoing_items_cost += flt(d.basic_amount)
 
-        return outgoing_items_cost
+        return outgoing_items_cost    
